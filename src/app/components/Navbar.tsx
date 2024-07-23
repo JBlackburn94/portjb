f19b4a6a8426e385a719aecf5606867055ed375d
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-07-23 22:49:39 +0100

    adds mobile nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,28 @@
+"use client";
+import { Squeeze } from "hamburger-react";
+import { useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
 export default function Navbar() {
-  return <div></div>;
+  const [nav, setNav] = useState(false);
+
+  useGSAP(() => {
+    gsap.to("#nav", {});
+  });
+
+  return (
+    <header className="bg-background text-main w-full fixed">
+      <nav className="p-[30px] flex justify-between items-center" id="nav">
+        <h1 className="font-bold">
+          Jason
+          <br />
+          Blackburn
+        </h1>
+        <Squeeze />
+      </nav>
+    </header>
+  );
 }
