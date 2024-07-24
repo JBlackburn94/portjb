a14b0c47de7c6425203e88feb3ce144fac91e264
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-07-24 16:32:00 +0100

    adds padding to the bottom of the main tag, adding needed space to the bottom of the page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,9 +2,14 @@ import Card from "../components/Card";
 
 export default function page() {
   return (
-    <main className="bg-background h-[100dvh] overflow-x-hidden text-main pt-[128px] flex flex-col items-center justify-start gap-[40px] px-[25px]">
+    <main className="bg-background h-[100dvh] overflow-x-hidden text-main pt-[128px] flex flex-col items-center justify-start gap-[40px] px-[25px] pb-4">
       <h2 className="font-bold text-[28px]">Portfolio</h2>
-      <section className="flex flex-col gap-[40px]">
+      <section className="flex flex-col gap-[60px]">
+        <Card
+          title="Project One"
+          tech="Next.js, TailwindCSS, GSAP, Git, Vercel"
+          description="An exmaple website for a car garage with built in features such as a price estimator etc."
+        />
         <Card
           title="Project One"
           tech="Next.js, TailwindCSS, GSAP, Git, Vercel"
